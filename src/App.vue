fc4c72daef15e5268a9d5c0e1b9c7158969e1b7b
Jyrone Parker <jyrone.parker@mgmail.com>
2022-03-20 19:12:37 -0400

    postcss config

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import './index.css'
+
 export default {
   name: 'App',
   components: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-// tailwind.config.js
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./public/**/*.html', './src/**/*.vue', './src/main.js'],
-    // ...
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
   },
-  // ...
-}
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
