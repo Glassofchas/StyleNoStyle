2b53e74f832ec2bfbe64ea12151c55485820787d
Jyrone Parker <jyrone.parker@mgmail.com>
2022-03-20 19:30:15 -0400

    fixed logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Style No Style logo" src="./assets/vector/logo.svg" class="w-1 h-1">
+    <img alt="Style No Style logo" src="./assets/vector/default-monochrome.svg" class="w-100 h-100">
     <HelloWorld msg="Style No Style!!"/>
   </div>
 </template>
